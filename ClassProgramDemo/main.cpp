5906c1e497d60db2e4f42520f5160b1a592005ed
Aliel <alielreyes@gmail.com>
2016-10-17 23:38:10 -0700

    added setters and getters to Person class name

--- ClassProgramDemo/main.cpp
+++ ClassProgramDemo/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 class Person 
@@ -9,6 +10,8 @@ public:
 	Person(): age(0), name("") {}
 	void setAge(int age);
 	int getAge();
+	void setName(string name);
+	const string& getName() const;
 };
 
 void Person::setAge(int age) 
@@ -21,11 +24,23 @@ int Person::getAge()
 	return age;
 }
 
+void Person::setName(string name) 
+{
+	this->name = name;
+}
+
+const string& Person::getName() const 
+{
+	return this->name;
+}
+
 int main() 
 {
 	cout << "Program first release is done" << endl;
 	Person p1;
 	p1.setAge(21);
+	p1.setName("Aliel");
+	cout << "Person name: " << p1.getName() << endl;
 	cout << "Person age: "<<p1.getAge() << endl;
 
 	system("Pause");
