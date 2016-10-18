31011ee30c40e59da505a5495aa41cba44cea03e
Aliel <alielreyes@gmail.com>
2016-10-17 23:12:05 -0700

    Finished for first release

--- ClassProgramDemo/main.cpp
+++ ClassProgramDemo/main.cpp
@@ -7,12 +7,26 @@ class Person
 	string name;
 public:
 	Person(): age(0), name("") {}
+	void setAge(int age);
+	int getAge();
 };
+
+void Person::setAge(int age) 
+{
+	this->age = age;
+}
+
+int Person::getAge() 
+{
+	return age;
+}
 
 int main() 
 {
 	cout << "Program first release is done" << endl;
 	Person p1;
+	p1.setAge(21);
+	cout << "Person age: "<<p1.getAge() << endl;
 
 	system("Pause");
 	return 0;
