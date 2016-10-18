3f0c2aacab2475d784219a6dce3ca362b9658491
Aliel <alielreyes@gmail.com>
2016-10-17 23:27:08 -0700

    Added new branch Contacts and added a class called Contacts on main

--- ClassProgramDemo/main.cpp
+++ ClassProgramDemo/main.cpp
@@ -21,12 +21,21 @@ int Person::getAge()
 	return age;
 }
 
+class Contacts 
+{
+	int pNumber;
+public:
+	Contacts(): pNumber(0) {}
+};
+
 int main() 
 {
 	cout << "Program first release is done" << endl;
 	Person p1;
 	p1.setAge(21);
 	cout << "Person age: "<<p1.getAge() << endl;
+	Contacts c1;
+	cout << "Contact Class added: " << endl;
 
 	system("Pause");
 	return 0;
